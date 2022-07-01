c41de9deecdc312b03f1826f8a3777f3bd244920
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2022-07-01 14:32:36 +0900

    応募機能実装

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -2,6 +2,7 @@ class Recruitment < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :category
+  has_many   :applies
   belongs_to :capacity
   belongs_to :end_time
   belongs_to :level
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'recruitments#index'
   get 'pages/show'
-  resources :recruitments, except: [:index]
+  resources :recruitments, except: [:index] do
+    resources :applies, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
