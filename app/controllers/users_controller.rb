2fb1a28efe1a320535c3be88641f70db292b6bbe
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2022-07-03 23:00:43 +0900

    レビュー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      redirect_to root_path
+    end
   end
 end
