270bd4063980e75928637f3383a49768209bcf99
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2022-07-04 17:24:54 +0900

    レビュー修正、ログイン状態でのアクセス制限実装

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -1,5 +1,5 @@
 class AppliesController < ApplicationController
-  before_action :authenticate_user!, only:[:index]
+  before_action :authenticate_user!, only:[:index, :new]
 
   def new
     @recruitment = Recruitment.find(params[:recruitment_id])
