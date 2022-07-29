319053cc46a7b85e5474649b786c098af8bb8707
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2022-07-29 13:14:45 +0900

    無断欠席時のプログラム実装

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -7,6 +7,7 @@ class EvaluationsController < ApplicationController
   end
 
   def create
+    # 評価相手のユーザーを特定
     agreement = Agreement.find(params[:agreement_id])
     apply = agreement.apply
     message_room = MessageRoom.find_by(apply_id: apply.id)
@@ -14,6 +15,19 @@ class EvaluationsController < ApplicationController
     user = User.find(entry.user.id)
     evaluation = Evaluation.find_by(user_id: user.id)
     evaluate(evaluation)
+  end
+
+  def absence_without_notice
+    # 評価相手のユーザーを特定
+    agreement = Agreement.find(params[:agreement_id])
+    apply = agreement.apply
+    message_room = MessageRoom.find_by(apply_id: apply.id)
+    entry = Entry.where(message_room_id: message_room.id).where.not(user_id: current_user.id).take
+    user = User.find(entry.user.id)
+    penalty_point = PenaltyPoint.find_by(user_id: user.id)
+    penalty_point.point += 10
+    penalty_point.save
+    penalty_point_log = PenaltyPointLog.create(increase_decrease_value: 10, penalty_point_id: penalty_point.id)
   end
 
   private
